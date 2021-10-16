877df8abb008131de21936923197e76db45e78cc
kabanooovd <kabanooovd@yandex.ru>
2021-10-16 12:36:12 +0300

    test commit

--- src/Components/PasswordRecovery/PasswordRecovery.tsx
+++ src/Components/PasswordRecovery/PasswordRecovery.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-
+let a = 'lalala'
 
 export const PasswordRecovery = () => {
     return (
         <div>
+            {alert(a)}
             PasswordRecovery
         </div>
     )
