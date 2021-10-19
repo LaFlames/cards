1e5f9d679bb97f773c67e94a258969bf9b997dc6
Vitali <vetalichegga@gmail.com>
2021-10-19 19:33:40 +0300

    add thunks/actions for login

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,12 +1,45 @@
 import React from 'react'
+import {useDispatch, useSelector} from "react-redux";
+import {AppRootStateType} from "../../app/store";
+import {UserProfileType} from "../Login/login-api";
+import {SuperButton} from "../SuperComponents/SuperButton/SuperButton";
+import {Redirect} from "react-router-dom";
+import {PATH} from "../Routes";
+import {unLoginUserTC} from "../Login/login-reducer";
 
 
 
 export const Profile = () => {
+
+    let dispatch = useDispatch()
+
+    let userProfile = useSelector<AppRootStateType, UserProfileType | null>(state => state.login.userProfile)
+    let isAuth = useSelector<AppRootStateType, boolean>(state => state.login.isAuth)
+    let {name, email, avatar, ...restProps} = {...userProfile}
+
+
+    const unLoginUseOnClick = () => {
+        dispatch(unLoginUserTC())
+    }
+
+
+    if (isAuth === false) {
+        return <Redirect to={PATH.LOGIN}/>
+    }
+
     return (
-        <div>
-            Profile
-        </div>
+        userProfile
+            ?
+                <div>
+                    <img src={avatar} alt="user-photo"/>
+                    <div>name: {name}</div>
+                    <div>email: {email}</div>
+                    <div>
+                        <SuperButton onClick={unLoginUseOnClick}>Log out</SuperButton>
+                    </div>
+                </div>
+            :
+                <div></div>
     )
 }
 
