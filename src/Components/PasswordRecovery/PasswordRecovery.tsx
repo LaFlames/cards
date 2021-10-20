a737393c0222475e300707ebf71b2aa9d08fa58b
Maxim Bordea <maxbordea@gmail.com>
2021-10-20 20:25:01 +0300

    feature: implement logic of password-recovery

--- src/Components/PasswordRecovery/PasswordRecovery.tsx
+++ src/Components/PasswordRecovery/PasswordRecovery.tsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import s from './PasswordRecovery.module.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppRootStateType } from '../../app/store'
+import { SuperButton } from '../SuperComponents/SuperButton/SuperButton'
+import { SuperInputText } from '../SuperComponents/SuperInputText/SuperInputText'
+import { PasswordRecoveryInitialStateType, setEmailForPasswordTC, setErrorMessageAC, setIsPasswordRecoverySucceededAC, setPasswordRecoveryAC } from './passwordRecovery-reducer'
 
 
 
 export const PasswordRecovery = () => {
+
+    const dispatch = useDispatch()
+    const email = useSelector<AppRootStateType, string>(state => state.passwordRecovery.email)
+    const state = useSelector<AppRootStateType, PasswordRecoveryInitialStateType>(state => state.passwordRecovery)
+    const isRequestSucceeded = useSelector<AppRootStateType, boolean>(state => state.passwordRecovery.isRequestSucceeded)
+    const errorMessage = useSelector<AppRootStateType, string>(state => state.passwordRecovery.errorMessage)
+    const isLoading = useSelector<AppRootStateType, boolean>(state => state.passwordRecovery.isLoading)
+
+
+    useEffect(() => {
+        return () => {
+            dispatch(setPasswordRecoveryAC(''))
+            dispatch(setErrorMessageAC(''))
+            dispatch(setIsPasswordRecoverySucceededAC(false))
+        }
+    }, [])
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        dispatch(setPasswordRecoveryAC(e.currentTarget.value))
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(e.target)
+        dispatch(setEmailForPasswordTC(state))
+    }
+
+    if (isRequestSucceeded) {
+        return <div className={s.mainBlock}>
+            <p>Success!<br/>click the link in the message in your email</p>
+        </div>
+    }
+
     return (
-        <div>
-            PasswordRecovery
+        <div className={s.mainBlock}>
+            <div className={s.title}>
+                <h1>Forgot your password?</h1>
+            </div>
+
+            <div>
+            <div className={s.loading}>
+                {isLoading ? <span>loading</span> : errorMessage}
+            </div>
+            <form onSubmit={handleSubmit}>
+                    <div>
+                        <SuperInputText type='email' placeholder='Email' onChange={onChange} value={email}/>
+                    </div>
+                    <div>
+                        <p>Enter your email address and we will send you futher instructions</p>
+                    </div>
+                    <div>
+                        <SuperButton disabled={isLoading}>Send Instructions</SuperButton>
+                    </div>
+                </form>
+            </div>
+
+            <div>
+                <p>Did you remember your password?</p>
+                <a href='/login'>Try logging in</a>
+            </div>
         </div>
     )
 }
--- src/Components/PasswordRecovery/passwordRecovery-reducer.ts
+++ src/Components/PasswordRecovery/passwordRecovery-reducer.ts
@@ -1,18 +1,75 @@
+import { Dispatch } from "redux"
+import { authAPI } from "./password-api"
 
+const initialState: PasswordRecoveryInitialStateType = {
+    isLoading: false,
+    errorMessage: '',
+    isRequestSucceeded: false,
+    email: 'ok',
+    from: "test-front-admin",
+    message: `<div style="background-color: lime; padding: 15px"> password recovery link: <a href='http://localhost:3000/friday-project#/add-new-password/$token$'> link</a></div>`		
+}
 
-
-
-type ActionsType = {}
-
-
-export type PasswordRecoveryInitialStateType = {}
-
-const initialState: PasswordRecoveryInitialStateType = {}
-
-export const passwordRecoveryReducer = (state = initialState, action: ActionsType): PasswordRecoveryInitialStateType => {
-    switch (action) {
-
+export const passwordRecoveryReducer = (state: PasswordRecoveryInitialStateType = initialState, action: ActionsType): PasswordRecoveryInitialStateType => {
+    switch (action.type) {
+        case 'PASSWORD-RECOVERY/EMAIL-IS-CHANGING': 
+            return {...state, email: action.email}
+        case 'PASSWORD-RECOVERY/IS-REQUEST-SUCCEEDED':
+            return {...state, isRequestSucceeded: action.isRequestSucceeded}
+        case 'PASSWORD-RECOVERY/ERROR-MESSAGE':
+            return {...state, errorMessage: action.errorMessage}
+        case 'PASSWORD-RECOVERY/IS-LOADING':
+            return {...state, isLoading: action.isLoading}
         default:
             return state
     }
+}
+
+//actions
+
+export const setPasswordRecoveryAC = (email: string) =>
+    ({type: 'PASSWORD-RECOVERY/EMAIL-IS-CHANGING', email} as const)
+export const setIsPasswordRecoverySucceededAC = (isRequestSucceeded: boolean) =>
+    ({type: 'PASSWORD-RECOVERY/IS-REQUEST-SUCCEEDED', isRequestSucceeded} as const)
+
+export const setIsLoadingAC = (isLoading: boolean) =>
+    ({type: 'PASSWORD-RECOVERY/IS-LOADING', isLoading} as const)
+export const setErrorMessageAC = (errorMessage: string) =>
+    ({type: 'PASSWORD-RECOVERY/ERROR-MESSAGE', errorMessage} as const)
+
+
+//thunk
+
+export const setEmailForPasswordTC = (data: PasswordRecoveryInitialStateType) => (dispatch: Dispatch) => {
+    dispatch(setIsLoadingAC(true))
+    authAPI.createNewPassword(data)
+        .then(res => {
+            dispatch(setIsPasswordRecoverySucceededAC(true))
+            dispatch(setIsLoadingAC(false))
+        })
+        .catch((e) => {
+            const error = e.response 
+                ? e.response.data.error 
+                : (e.message + ', more details in the console')
+
+            dispatch(setErrorMessageAC(error))
+            dispatch(setIsLoadingAC(false))
+        })
+}
+
+//types
+
+type ActionsType = 
+    | ReturnType<typeof setPasswordRecoveryAC>
+    | ReturnType<typeof setIsPasswordRecoverySucceededAC>
+    | ReturnType<typeof setErrorMessageAC>
+    | ReturnType<typeof setIsLoadingAC>
+
+export type PasswordRecoveryInitialStateType = {
+    isLoading: boolean
+    errorMessage: string
+    isRequestSucceeded: boolean
+    email: string
+    from: string
+    message: string
 }
